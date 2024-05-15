0ed3e3b3e68a86554b26816570a2c92fc6fd0a44
Esteban Huanca Velarde <ehuancav132005@gmail.com>
2024-05-15 16:51:30 -0300

    Arrays: 4/6 temas leidos

---
+++ Arrays/2-metodos.js
@@ -0,0 +1,18 @@
+//En un restaurante se reciben pedidos de comida a domicilio. Vamos a escribir una función procesarPedido que 
+//recibe un pedido, que es un array de platos. Lo que debemos hacer es:
+    //El primer elemento lo sacamos del array, ya que es el nombre del cliente.
+    //Añadimos al principio del array la cadena de texto "bebida", ya que es una promoción que tenemos.
+    //Después añadimos al final del array el nombre del usuario que sacamos antes.
+
+
+const pedidos = ["Juan", "Hamburgesas", "Papas Fritas", "Pizza"]
+
+function procesar_Pedido(pedidos) {
+    const nombreCliente = pedidos.shift();
+    pedidos.unshift("bebida");
+    pedidos.push(nombreCliente);
+
+    return pedidos;
+}
+
+console.log(procesar_Pedido(pedidos));
---
+++ Arrays/3-bucles.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3, 4, 5, 6]
+let contador = 0
+
+//while
+while (contador < numbers.length) {
+    console.log(numbers[contador])
+    contador++
+}
+
+//for
+ 
+
+//funcion flecha
+
+//función regular
+
+//forEach
+
